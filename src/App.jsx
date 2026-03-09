6f694c8112b1edc3c93a6917feaa3a6c5ccb280f
zaitounish <mohamed.taher1888@gmail.com>
2026-03-09 07:00:54 +0200

    Fixed analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect, useMemo } from "react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import {
   UploadCloud,
   Trash2,
